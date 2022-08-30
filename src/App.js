062488af60055895bd762b9fe0a79650dfd3d206
candela.avalos.cirullo <avaloscirullocande@gmail.com>
2022-08-30 19:03:34 -0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Conteiner from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div className='App'>
+            <Navbar>
+                <Conteiner>
+                    <Navbar.Brand className='logo' href='#home'>iShop</Navbar.Brand>
+                    <Nav className='me-auto'>
+                        <NavLink className='menu' href='#home'> Home</NavLink>
+                        <NavLink className='menu' href='#features'> iPhone</NavLink>
+                        <NavLink className='menu' href='#pricing'>Samsung</NavLink>
+                    </Nav>
+                </Conteiner>
+            </Navbar>
+
+        </div>
+    )
 }
 
 export default App;
